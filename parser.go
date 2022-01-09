dee6d2e9568d4a4e970c0fc1a1f609ff8ad9d9fc
OLUWAMUYIWA <onigbindemy@gmail.com>
2022-01-09 13:34:26 +0100

    continued

--- parser.go
+++ parser.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"unicode/utf8"
 )
 
 type ParserErr struct {
@@ -63,4 +64,38 @@ func (p *Parser) Peep() (byte, error) {
 		return 0, fmt.Errorf("nothing more to read")
 	}
 	return p.b[p.pos], nil
+}
+
+type BasicParser func(in ParserInput) ParserRes
+type CombFunc func()
+type Combinator func(in ParserInput)
+type ParserInput interface {
+	CurrUCodePoint() rune
+	Rem() ParserInput
+}
+
+type ParserRes struct {
+	res interface{}
+	rem ParserInput
+}
+
+var digit BasicParser = func(in ParserInput) ParserRes {
+	curr := in.CurrUCodePoint()
+	//if curr is between  and 9
+	if rune('0') <= curr && rune('9') >= curr {
+		return ParserRes{
+			curr, in.Rem(),
+		}
+	}
+	return ParserRes{nil, in}
+}
+
+var character BasicParser = func(in ParserInput) ParserRes {
+	curr := in.CurrUCodePoint()
+	if utf8.ValidRune(curr) {
+		return ParserRes{
+			curr, in.Rem(),
+		}
+	}
+	return ParserRes{nil, in}
 }
