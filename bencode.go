15ec2a90b2d1064d1319544cc1a6884cc908af79
Oluwamuyiwa Onigbinde <onigbindemy@gmail.com>
2022-02-21 23:41:15 +0100

    update

--- bencode.go
+++ bencode.go
@@ -1,10 +1,29 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"unicode/utf8"
+)
+
 type Bencode struct {
 }
 
 type Encoder struct {
+	data []byte
 }
 
 type Decoder struct {
+	data []byte
+}
+
+
+
+func (d *Decoder) Decode() {
+	b := bufio.NewScanner(bytes.NewBuffer((*d).data))
+	b.Split(bufio.ScanRunes)
+	for b.Scan() {
+		r, _ :=  utf8.DecodeRune(b.Bytes())
+		
+	}
 }
